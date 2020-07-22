93323f09784cf008d64a97848fc87acb9255a517
lazychun1299 <13871405571@163.com>
2020-07-22 15:35:30 +0800

    update PddLogistics.js

--- examples/src/component/PddLogistics.js
+++ examples/src/component/PddLogistics.js
@@ -1,10 +1,61 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet, ScrollView, View, Text} from 'react-native';
-
+import {StyleSheet, View, Text, Image} from 'react-native';
+import Steps from 'react-native-logistics-steps';
 import logisticsData from '../mockData/pdd';
 
-const PddLogistics = () => {
-  return <Text>UUKKKKNOPPP</Text>;
+const CurrentStepIndicator = () => {
+  return (
+    <Image
+      style={styles.currentStepIndicatorStyle}
+      source={require('../images/pdd/check.png')}
+    />
+  );
 };
+
+const StepIndicator = () => {
+  return <View style={styles.stepIndicatorStyle} />;
+};
+
+const LogisticContent = ({item}) => {
+  return (
+    <View>
+      <Text>{item.status}</Text>
+      <Text>{item.time}</Text>
+    </View>
+  );
+};
+
+const PddLogistics = () => {
+  return (
+    <View style={styles.container}>
+      <Steps
+        currentStepIndex={0}
+        items={logisticsData.list}
+        stepIndicatorMarginTop={2}
+        currentStepIndicator={<CurrentStepIndicator />}
+        stepIndicator={<StepIndicator />}
+        renderStepContent={(item, index) => (
+          <LogisticContent key={index} item={item} />
+        )}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+  },
+  stepIndicatorStyle: {
+    width: 16,
+    height: 16,
+    borderRadius: 8,
+    backgroundColor: '#d9d9d9',
+  },
+  currentStepIndicatorStyle: {
+    width: 16,
+    height: 16,
+  },
+});
 
 export default PddLogistics;
